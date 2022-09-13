6fe63e44488952d72d90fb04d5fbfe4a18ebfa03
katarinagresova <514001@mail.muni.cz>
2022-09-13 17:57:43 +0200

    WIP: clustering

---
+++ src/deepexperiment/clustering.py
@@ -0,0 +1,21 @@
+from sklearn.cluster import AgglomerativeClustering
+import tensorflow as tf
+
+def cluster_attention(attentions, dist_function, n_clusters):
+    dist = []
+    for i in range(len(attentions)):
+        row = []
+        for j in range(len(attentions)):
+            row.append(dist_function(attentions[i], attentions[j]))
+        dist.append(row)
+    clustering = AgglomerativeClustering(n_clusters=n_clusters, affinity="precomputed", linkage='complete').fit(dist)
+    return clustering.labels_
+
+def compute_cluster_consensus(attentions, labels):
+  cluster_consensus = []
+  for num in range(len(set(labels))):
+    cluster = attentions[labels == num].sum(axis=0)
+    cluster = tf.maximum(cluster, 0) / tf.math.reduce_max(cluster)
+    cluster_consensus.append(cluster)
+
+  return cluster_consensus
